b0dbda07016346cd0e6a225deba5f76f60ece4e2
AbdusamadAbdurahimov <survolgeyms@gmail.com>
2024-05-28 12:17:04 +0500

    JS-1-dars
    ZeroMax-IT Center-35-guruh

---
+++ Js/main.js
@@ -0,0 +1,28 @@
+// console.log("Men faqat konsol oynasida ko'rinaman!");
+
+// alert("Saytimizga xush kelibsiz!");
+
+// document.write(19+90+40+29*12*4*7*2/53);
+
+// let x =5;
+// let y =7;
+// let z =10;
+
+// let x = 15, y =7, z = 10;
+
+// document.write(x + y + z);
+
+// let son =45;
+// let matn = "JavaScript";
+
+// document.write(typeof matn);
+let x =5;
+// x-=6;
+// x=5*5
+let daraja = x**3;
+
+document.write(daraja);
+
+
+
+
